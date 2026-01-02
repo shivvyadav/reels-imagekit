264655451b2f29e1efea3106f1e1f56318f56142
Shiv Yadav <shivyadav0014@gmail.com>
2026-01-02 22:49:43 +0545

    add bcrypt for user password encryption

--- src/models/User.ts
+++ src/models/User.ts
@@ -26,7 +26,11 @@ const userSchema = new Schema<IUser>(
   {timestamps: true}
 );
 
-
+// encrypt password
+userSchema.pre<IUser>("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
 
 const User = mongoose.models.User || mongoose.model<IUser>("User", userSchema);
 export default User;

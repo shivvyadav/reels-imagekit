256bbb5bdb4a6b7ad874a0e2b1acdca306aef173
Shiv Yadav <shivyadav0014@gmail.com>
2026-01-04 09:06:19 +0545

    configured react hot toast other providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
+import {Providers} from "@/components/Providers";
+import Header from "@/components/Header";
+import {Toaster} from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +26,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang='en'>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Providers>
+          <Header />
+          <main className='container mx-auto px-4 py-8'>{children}</main>
+          <Toaster />
+        </Providers>
       </body>
     </html>
   );
